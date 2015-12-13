c3201e169be6f1339a69864b09dc040f3ecffaa3
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-13 15:54:52 -0800

    Added dynamic typing example

--- Ry Objective-C Tutorial/main.m
+++ Ry Objective-C Tutorial/main.m
@@ -140,8 +140,43 @@ int main(int argc, const char * argv[]) {
     
     Car *chevy = [[Car alloc] initWithModel:@"Chevy Corvette"];
     NSLog(@"Created a %@, too.", chevy.model);
+    NSLog(@"\n\n");
     //
     
+    //Dynamic Typing
+    Car *delorean = [[Car alloc] initWithModel:@"DeLorean"];
+    
+    // Get the class of an object
+    NSLog(@"%@ is an instance of the %@ class",
+          [delorean model], [delorean class]);
+    
+    // Check an object against a class and all subclasses
+    if ([delorean isKindOfClass:[NSObject class]]) {
+        NSLog(@"%@ is an instance of NSObject or one "
+              "of its subclasses",
+              [delorean model]);
+    } else {
+        NSLog(@"%@ is not an instance of NSObject or "
+              "one of its subclasses",
+              [delorean model]);
+    }
+    
+    // Check an object against a class, but not its subclasses
+    if ([delorean isMemberOfClass:[NSObject class]]) {
+        NSLog(@"%@ is a instance of NSObject",
+              [delorean model]);
+    } else {
+        NSLog(@"%@ is not an instance of NSObject",
+              [delorean model]);
+    }
+    
+    // Convert between strings and classes
+    if (NSClassFromString(@"Car") == [Car class]) {
+        NSLog(@"I can convert between strings and classes!");
+    }
+    NSLog(@"\n\n");
+    //
+
     return 0;
 }
 
